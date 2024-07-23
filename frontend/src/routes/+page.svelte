ae3d0b6f84ceb0de8aaa20d956959dc9d9cecc4e
Allan K. Koech <allankoech.ke@gmail.com>
2024-07-23 14:58:54 +0300

    Got frontend login/task submit and status check working

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,37 +1,97 @@
 <script>
-    import prepareJob from "$lib/index.js";
     import Modal from "./Modal.svelte";
 
     let jobs = [];
-    let clientID = "";
-    let clientSecret = "";
+    let clientID = "EaWFXup2bfhWIrCALMOmgqFLqs7Y9Qq6FPYmG7F3PP0o13wg";
+    let clientSecret = "0zb6od47E9ZPAGe5AWqCPecKCDVkb6SzFB1NisxV0WwlPAGgmwphkaLIJfGFYZVU";
+    let token = "";
     let showModal = false;
-
+    let isAuthenticated = false;
 
     async function startNewJob() {
         if (validateFiles()) {
             console.log("Working OKAY");
 
-            const files = document.getElementById("fileUpload").files;
+            const fileInput = document.getElementById("fileUpload");
             const name = document.getElementById("taskName").value;
 
-            var ntask = {
-                uuid: "",
-                name: name,
-                files: files,
-                status: "INIT"
+            const formData = new FormData();
+
+            // Assuming fileInput is an <input type="file" multiple> element
+            for (const file of fileInput.files) {
+                formData.append('taskFiles', file);
             }
 
-            jobs = jobs.push(ntask)
-            prepareJob(ntask, clientID, clientSecret, (obj) => {
-                console.log(JSON.stringify(obj))
-            })
+            formData.append('taskName', name);
+
+            try {
+                const response = await fetch('http://127.0.0.1:3000/task', {
+                    method: 'POST',
+                    body: formData,
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+
+                const data = await response.json();
+
+                console.log(data)
+
+                var newJob = {
+                    jobId: data.jobId,
+                    queueId: data.queueId,
+                    taskName: name,
+                    status: "QUEUED"
+                }
+
+                jobs = [...jobs, newJob]
+
+                const intervalId = setInterval(() => {
+                    // Call the function and pass the handle (intervalId) to it
+                    checkStatus(data.jobId, data.queueId, intervalId);
+                }, 5000);
+
+            } catch (error) {
+                alert('There has been a problem with your fetch operation:', error)
+                console.error('There has been a problem with your fetch operation:', error);
+            }
         }
+
+        async function checkStatus(jobId, queueId, intervalId) {
+            const response = await fetch("http://127.0.0.1:3000/status", {
+                method: "POST",
+                body: JSON.stringify({ jobId, queueId }),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`,
+                },
+            });
+
+            if(!response.ok) {
+                alert("Check Status Failed")
+                // console.log(response.error())
+                clearInterval(intervalId);
+            }
+            
+            else {
+                const data = await response.json()
+                jobs = jobs.map(job => job.jobId === jobId ? { ...job, status: data.status} : job);
+
+                if (data.status === 'SUCCEEDED' || data.status === 'FAILED' || data.status === 'CANCELED') {
+                    clearInterval(intervalId);
+                }
+            }
+        }
+
     }
 
     function validateFiles(event) {
         const name = document.getElementById("taskName").value;
-        if( name === null || name.length === 0 ) {
+        if (name === null || name.length === 0) {
             alert("Task job is required");
             event.preventDefault();
             return false;
@@ -64,12 +124,36 @@
 
         return true;
     }
+
+    async function authenticateUser() {
+        if (clientID === "" || clientSecret === "") {
+            alert("Client ID and Secret is required");
+            return;
+        }
+
+        const response = await fetch("http://127.0.0.1:3000/login", {
+            method: "POST",
+            body: JSON.stringify({ clientID, clientSecret }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if(!response.ok) {
+            console.log(response.error())
+        } else {
+            const data = await response.json()
+            // console.log(data)
+            isAuthenticated = true;
+            token = data.token;
+        }
+    }
 </script>
 
 <div class="flex-1 flex-col bg-gray-200 m-6 rounded-md p-8">
     <h1 class="pt-4 pb-4 font-medium">Client Details</h1>
     <div class="flex row justify-between align-center">
-        <div class="flex row">
+        <div class="flex row gap-2 items-center">
             <input
                 bind:value={clientID}
                 type="password"
@@ -83,12 +167,27 @@
                 placeholder="Client Secret"
                 class="pt-2 pb-2 pl-4 pr-4 rounded-sm ml-2"
             />
+
+            {#if isAuthenticated}
+                <div
+                    class="text-green-800 text-bold border-2 rounded-md border-green-800 h-full flex flex-row items-center justify-center pl-4 pr-4"
+                >
+                    Authenticated
+                </div>
+            {:else}
+                <button
+                    class="pt-2 pb-2 pl-4 pr-4 bg-green-800 text-white"
+                    on:click={authenticateUser}>Authenticate</button
+                >
+            {/if}
         </div>
         <div class="flex row spacing-1">
-            <button
-                class="pt-2 pb-2 pl-4 pr-4 bg-green-800 text-white"
-                on:click={() => (showModal = true)}>Add Task</button
-            >
+            {#if isAuthenticated}
+                <button
+                    class="pt-2 pb-2 pl-4 pr-4 bg-green-800 text-white"
+                    on:click={() => (showModal = true)}>Add Task</button
+                >
+            {/if}
         </div>
     </div>
     <div class="h-[1px] w-full bg-gray-400 mt-4 mb-4" />
@@ -107,8 +206,8 @@
             {#each jobs as job}
                 <tr>
                     <td class="lp-2 rp-2 tp-1 bp-1">No.</td>
-                    <td class="lp-2 rp-2 tp-1 bp-1">{job.name}</td>
-                    <td class="lp-2 rp-2 tp-1 bp-1">{job.files}</td>
+                    <td class="lp-2 rp-2 tp-1 bp-1">{job.taskName}</td>
+                    <td class="lp-2 rp-2 tp-1 bp-1">{job.jobId}</td>
                     <td class="lp-2 rp-2 tp-1 bp-1">{job.status}</td>
                     <td class="lp-2 rp-2 tp-1 bp-1">View</td>
                 </tr>
@@ -123,8 +222,11 @@
         class="flex flex-row flex-1 justify-between items-center"
     >
         <h2 class="pb-2 pt-2 font-medium">New Task</h2>
-        <button on:click={() => { showModal = false; }} class="w-[30px] h-[30px] text-red-500"
-            >x</button
+        <button
+            on:click={() => {
+                showModal = false;
+            }}
+            class="w-[30px] h-[30px] text-red-500">x</button
         >
     </div>
 
--- server/server.js
+++ server/server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-const axios = require('axios');
+const axios = require('axios').default;
 const fs = require('fs');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const _ = require('lodash');
+const aps = require("./flow-grap-engine")
 
 
 const app = express();
 
+let APS_ACCESS_TOKEN = ""
+let data = []
+
+// Middleware to extract access token
+function extractToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        req.token = token;
+        next();
+    } else {
+        res.status(401).send('Access token is missing');
+    }
+}
 
 // enable files upload
 app.use(fileUpload({
@@ -21,7 +36,7 @@ app.use(fileUpload({
 //add other middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Public folder
@@ -58,24 +73,55 @@ function validateFiles(req, res) {
     return true;
 }
 
+app.post('/login', async (req, res) => {
+    if (!req.body.clientID) {
+        return res.send({
+            status: false,
+            message: 'Client ID is required'
+        });
+    }
+
+    if (!req.body.clientSecret) {
+        return res.send({
+            status: false,
+            message: 'Client Secret is required'
+        });
+    }
+
+    const accessToken = await aps.getOauthToken(req.body.clientID, req.body.clientSecret);
+
+    if (accessToken === "")
+        return res.status(403).send({
+            status: false,
+            message: 'Client log in failed'
+        });
+
+    else
+        return res.status(200).send({
+            status: true,
+            message: 'Logged in',
+            token: accessToken
+        });
+});
+
 // Upload endpoint
-app.post('/task', async (req, res) => {
+app.post('/task', extractToken, async (req, res) => {
     try {
         if (!req.body.taskName) {
             return res.send({
                 status: false,
                 message: 'Task name not provided (taskName)'
             });
-        } 
+        }
 
         if (!req.files) {
             return res.send({
                 status: false,
                 message: 'No files uploaded'
             });
-        } 
+        }
 
-        if(!validateFiles(req, res)) {
+        if (!validateFiles(req, res)) {
             return res.send({
                 status: false,
                 message: 'Required files/inputs missing! Please select one .json file and one .usd file.'
@@ -87,21 +133,151 @@ app.post('/task', async (req, res) => {
             let bifrostGraph = files[0].name.endsWith(".json") ? files[0] : files[1]
             let inputUsd = files[0].name.endsWith(".usd") ? files[0] : files[1]
 
-            bifrostGraph.mv('./uploads/' + bifrostGraph.name);
-            inputUsd.mv('./uploads/' + inputUsd.name);
+            let now = Date.now()
+            let bifrostGraphName = `${now}-${bifrostGraph.name}`
+            let inputUsdName = `${now}-${inputUsd.name}`
+
+            bifrostGraph.mv('./uploads/' + bifrostGraphName);
+            inputUsd.mv('./uploads/' + inputUsdName);
+
+            access_token = req.token
+            return await prepareRequest(req, res, access_token, path.join(__dirname, `../uploads/${bifrostGraphName}`), path.join(__dirname, `../uploads/${inputUsdName}`))
 
             //send response
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                }
-            });
+            // res.send({
+            //     status: true,
+            //     message: 'File is uploaded',
+            //     data: {
+            //     }
+            // });
         }
     } catch (err) {
         res.status(500).send(err);
     }
 });
+
+app.post('/status', extractToken, async (req, res) => {
+    try {
+        if (!req.body.jobId) {
+            return res.send({
+                status: false,
+                message: 'Job Id Required'
+            });
+        }
+
+        if (!req.body.queueId) {
+            return res.send({
+                status: false,
+                message: 'Queue ID required'
+            });
+        }
+
+        let job = await aps.getJob(req.token, req.body.queueId, req.body.jobId);
+        console.log(job)
+
+        if (job.status !== 'SUCCEEDED' && job.status !== 'FAILED' && job.status !== 'CANCELED') {
+            return res.status(200).send({
+                status: job.status,
+                logs: [],
+                outputs: [],
+                error: null
+            })
+        }
+
+        let queueId = req.body.queueId;
+        let jobId = req.body.jobId;
+
+        const ts = Date.now();
+        var logsArray = [];
+        var outputArray = [];
+        var resObj = { status: job.status, error: null, outputs: [], logs: [] };
+
+        // Downloading logs for the job
+        const logsDirectory = path.join(__dirname, `../.outputs/${ts}/.logs`);
+        console.log(`Downloading logs in ${logsDirectory}`);
+        aps.createDirectory(logsDirectory);
+        const logs = await aps.getLogs(req.token, queueId, jobId);
+        logs.results.forEach(async (result, index) => {
+            const downloadUrl = await aps.getDownloadUrlForResource(req.token, result.spaceId, result.resourceId);
+            let log = path.join(logsDirectory, `log_${index}.log`)
+            resObj.logs.push(log.toString())
+            await aps.downloadFileFromSignedUrl(downloadUrl.url, log);
+        });
+
+        // Downloading outputs for the job
+        const outputsDirectory = path.join(__dirname, `../.outputs/${ts}`);
+        console.log(`Downloading outputs in ${outputsDirectory}`);
+        aps.createDirectory(outputsDirectory);
+        const outputs = await aps.getOutputs(req.token, queueId, jobId);
+        outputs.results.forEach(async (result, index) => {
+            try {
+                const downloadUrl = await aps.getDownloadUrlForResource(req.token, result.spaceId, result.resourceId);
+                const outputFile = path.join(outputsDirectory, `output_${index}.usd`);
+                resObj.logs.push(outputFile.toString())
+                await aps.downloadFileFromSignedUrl(downloadUrl.url, outputFile);
+                console.log(outputFile, outputFile.toString())
+            } catch (err) {
+                console.log("Output Error: ", err)
+            }
+        });
+
+        if (job.status === 'FAILED') {
+            const taskExecutions = await aps.getTaskExecutions(req.token, queueId, jobId);
+            const taskError = taskExecutions?.results?.[0].error;
+            if (taskError) {
+                console.log(JSON.stringify(taskError));
+                resObj.error = taskError;
+            }
+        }
+
+        console.log('Done')
+        console.log(resObj)
+
+        return res.status(200).send(resObj)
+
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+async function prepareRequest(req, res, access_token, bifrostGraphPath, inputFilePath) {
+    const storageSpaceId = 'scratch:@default';
+
+    // use the personal queue for our app
+    const queueId = '@default';
+
+    // Upload input file (plane.usd)
+    console.log('Uploading input file');
+    const getInputFileUploadUrlResponse = await aps.getResourceUploadUrl(access_token, storageSpaceId, path.parse(inputFilePath).base);
+    const inputFileEtag = await aps.uploadToSignedUrl(getInputFileUploadUrlResponse.urls[0].url, inputFilePath);
+    const inputFileUrn = await aps.completeUpload(access_token, storageSpaceId, getInputFileUploadUrlResponse.upload.resourceId, getInputFileUploadUrlResponse.upload.id, inputFileEtag);
+    console.log('Input File uploaded');
+
+    // Upload bifrost graph file (addTrees.json)
+    console.log('Uploading bifrost graph file');
+    const getGraphUploadUrlResponse = await aps.getResourceUploadUrl(access_token, storageSpaceId, path.parse(bifrostGraphPath).base);
+    const bifrostGraphEtag = await aps.uploadToSignedUrl(getGraphUploadUrlResponse.urls[0].url, bifrostGraphPath);
+    const bifrostGraphUrn = await aps.completeUpload(access_token, storageSpaceId, getGraphUploadUrlResponse.upload.resourceId, getGraphUploadUrlResponse.upload.id, bifrostGraphEtag);
+    console.log('Bifrost graph file uploaded');
+
+    // Submit job
+    let taskName = req.body.taskName
+    console.log("Task Name: ", req.body.taskName)
+    const jobId = await aps.submitJob(access_token, queueId, bifrostGraphUrn, inputFileUrn, { bifrostGraphPath, inputFilePath, taskName });
+
+    if (!jobId) {
+        console.log("Job submission task failed")
+        return res.status(500).send({
+            message: "Job submission failed, internal error"
+        });
+    }
+
+    console.log(`Job submitted, id: ${jobId}`);
+    return res.status(200).send({
+        jobId,
+        queueId
+    });
+};
 
 const PORT = 3000;
 app.listen(PORT, () => {
